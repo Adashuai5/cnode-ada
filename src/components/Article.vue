75844961a3cae6c5648d123a79f8449ef2cd4dee
adashuai5 <ad873606339@gmail.com>
2018-12-04 21:28:25 +0800

    移动过滤器至 main.js

---
+++ src/components/Article.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="Article">
+        <div class="loading" v-if="isLoading">
+            <img src="../assets/loading.gif">
+        </div>
+        <div v-else>
+            <div class="topic_header">
+                <div class="topic_title">{{post.title}}</div>
+                <ul>
+                    <li>•发布于：{{post.create_at | formatDate}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Aricle",
+        data(){
+            return {
+                isLoading: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
