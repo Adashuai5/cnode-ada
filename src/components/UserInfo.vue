5085211c2effddac8962421e38a0c0693b6e043f
adashuai5 <ad873606339@gmail.com>
2018-12-05 23:41:50 +0800

    UserInfo 初步完成逻辑

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,198 @@
+<template>
+    <div class="UserInfo">
+        <div class="loading" v-if="isLoading">
+            <img src="../assets/loading.gif">
+        </div>
+        <div v-else class="userInfomation">
+            <div class="header">
+                <a href="/">主页</a>
+                <span class="divider">/</span>
+            </div>
+            <section class=".clearfix">
+                <img :src="userinfo.avatar_url" alt="头像">
+                <span class="loginname">{{userinfo.loginname}}</span>
+                <p class="user_profile">{{userinfo.score}} 积分</p>
+                <p class="col_fade">注册时间 {{userinfo.create_at | formatDate}}</p>
+            </section>
+            <div class="replies">
+                <p>最近创建的话题</p>
+
+                <ul>
+                    <li v-for="item in userinfo.recent_topics" :key="item.id">
+                        <router-link :to="{name:'user_info',params:{
+                            name: item.author.loginname
+                            }
+                        }">
+                            <img :src="item.author.avatar_url">
+                        </router-link>
+                        <router-link :to="{name:'post_content',params:{
+                                id: item.id
+                            }
+                        }">
+                            {{item.title}}
+                        </router-link>
+                        <span class="last_reply">{{item.last_reply_at | formatDate}}</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="replies">
+                <p>最近参与的话题</p>
+                <ul>
+                    <li v-for="item in userinfo.recent_replies" :key="item.id">
+                        <router-link :to="{name:'user_info',params:{
+                            name: item.author.loginname
+                            }
+                        }">
+                            <img :src="item.author.avatar_url">
+                        </router-link>
+                        <router-link :to="{name:'post_content',params:{
+                                id: item.id
+                            }
+                        }">
+                            {{item.title}}
+                        </router-link>
+                        <span class="last_reply">{{item.last_reply_at | formatDate}}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserInfo",
+        data() {
+            return {
+                isLoading: false,
+                userinfo: {}
+            }
+        },
+        beforeMount() {
+            this.isLoading = true
+            this.getUserInfoData()
+        },
+        methods: {
+            getUserInfoData() {
+                this.$axios.get(`https://cnodejs.org/api/v1/user/${this.$route.params.name}`)
+                    .then(res => {
+                        this.isLoading = false
+                        this.userinfo = res.data.data
+                        // console.log(res);
+                    })
+                    .catch((err) => {
+                        alert(err);
+                    })
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .UserInfo {
+        .userInfomation {
+            background: white;
+            width: 75%;
+            margin: 10px auto;
+            .header {
+                padding: 10px;
+                background-color: #f6f6f6;
+                border-radius: 3px 3px 0 0;
+                a {
+                    color: #80bd01;
+                    text-decoration: none;
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                }
+                .divider {
+                    padding: 0 5px;
+                    color: #ccc;
+                }
+            }
+            clearfix::after {
+                content: '';
+                display: block;
+                clear: both;
+            }
+            section {
+                font-size: 14px;
+                padding: 10px;
+                border-top: 1px solid #e5e5e5;
+                border-radius: 0 0 3px 3px;
+                img {
+                    width: 40px;
+                    margin-right: 10px;
+                    float: left;
+                }
+
+                .loginname {
+                    color: #778087;
+                }
+
+                .user_profile {
+                    padding: 10px;
+                    margin-top: 20px;
+                    clear: left;
+                }
+
+                .col_fade {
+                    color: #ababab;
+                    margin-bottom: 10px;
+                }
+            }
+
+            img {
+                border-radius: 3px;
+                height: 30px;
+                width: 30px;
+                vertical-align: middle;
+            }
+
+            li {
+                list-style: none;
+            }
+
+            .replies,
+            .topics {
+                font-size: 0.72rem;
+                border-top: 10px #DDDDDD solid;
+            }
+
+            .last_reply {
+                text-align: right;
+                min-width: 50px;
+                display: inline-block;
+                white-space: nowrap;
+                float: right;
+                color: #778087;
+                font-size: 12px;
+            }
+
+            & > div {
+                p {
+                    padding: 12px 0 12px 12px;
+                    background-color: rgba(212, 205, 205, 0.17);
+                    font-size: 0.75rem;
+                    margin: 0;
+                }
+                > ul > li {
+                    padding: 4px 0 4px 12px;
+                    white-space: nowrap;
+                    font-size: 0.72rem;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    line-height: 30px;
+                    vertical-align: middle;
+                    a {
+                        color: #094E99;
+                        text-decoration: none;
+                        &:hover {
+                            text-decoration: underline;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
